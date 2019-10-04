6282245efefe1e5f5b44ac0ce91958436d4ffeb9
Jesse Mapel <jmapel@usgs.gov>
2019-10-04 15:29:43 -0700

    Added c++ library tests and fixed install path on linux (#3)
    * Added c++ library tests
    
    * Upped build number
    
    * MNT: Re-rendered with conda-build 3.18.9, conda-smithy 3.5.0, and conda-forge-pinning 2019.10.01
    
    * Added flag to install into lib instead of lib64
    
    * Fixed dumb copy-paste error

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -e # Abort on error
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTS=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTS=OFF ..
 cmake --build . --target install
 cd ..
 pip install . --no-deps -vv
