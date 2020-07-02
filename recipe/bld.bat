968de31e534da23904b17aaf4e5cb58e69ec32d1
Jesse Mapel <jmapel@usgs.gov>
2020-07-02 10:51:53 -0700

    Adds windows build (#25)
    * Testing windows build
    
    * Tick build #
    
    * Accidentally axes the compiler
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.3, and conda-forge-pinning 2020.07.01.21.17.12
    
    * Fixed windows cmake flags
    
    * Testing proper cmake patch
    
    * Trying more stringeny virtualenv support
    
    * Less stringent
    
    * Trying everything now
    
    * Removed interp
    
    * Removed new Vector.cpp file
    
    * Windows release build
    
    * Added setup version patch
    
    * Testing less specification gain
    
    * Final patch update
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

---
+++ recipe/bld.bat
@@ -0,0 +1,22 @@
+mkdir build
+cd build
+
+cmake   -GNinja ^
+        -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+        -DALE_BUILD_TESTS=OFF ^
+        -DALE_USE_EXTERNAL_JSON=ON ^
+        -DALE_USE_EXTERNAL_EIGEN=ON ^
+        -DCMAKE_BUILD_TYPE=Release ^
+        -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+        -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
+        -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
+        -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
+        -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
+        ..
+
+cmake --build . --target install --config Release
+
+cd ..
+pip install . --no-deps -vv
+
+if errorlevel 1 exit 1
